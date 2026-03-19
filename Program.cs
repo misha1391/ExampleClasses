195038c3b3f26df00a3e604915cfbfc3cf4b77ff
misha1391 <misha.martynov2009.1@gmail.com>
2026-03-19 20:30:23 +0300

    v1

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿namespace ExampleClasses
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public void SetName(string name) { this.name = name; }
+        public void SetAge(int age) { this.age = age; }
+        public string GetName() { return name; }
+        public int GetAge() { return age; }
+        public void SayHello()
+        {
+            Console.WriteLine($"Дароу, меня обзывают {name}, мне {age} годиков");
+        }
+    }
+    class NewPerson
+    {
+        private string name {get; set;} // Автоматическая генерация get, set (Не рекомендуется)
+        private int age {get; set;}
+        public NewPerson(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public int Age // По этому названию методу будет вызываться get и set автоматически
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0) age = value;
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+               if (value.Length > 3) name = value;
+            }
+        }
+        public void SayHello()
+        {
+            Console.WriteLine($"Дароу, меня обзывают {name}, мне {age} годиков");
+        }
+    }
+    class Program
+    {
+        static void PrintInfo(string name, int age)
+        {
+            Console.WriteLine($"name={name}");
+            Console.WriteLine($"age={age}");
+        }
+        static int FindThirdAngle(int angle1, int angle2)
+        {
+            int angle3 = 180 - angle1 - angle2;
+            return angle3;
+        }
+        static string ConToString(string str1, string str2)
+        {
+            return str1 + str2;
+        }
+        static List<int> SquareAllElements(List<int> lst)
+        {
+            var result = new List<int>();
+            foreach(var i in lst)
+            {
+                result.Add(i*i);
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            PrintInfo("misha", 16);
+            PrintInfo(ConToString("Python", " is best"), 0);
+            var elem = SquareAllElements([1, 2, 3, 4]);
+            foreach(var i in elem)
+            {
+                Console.WriteLine(i);
+            }
+            Person per1 = new Person("Дэбил", 24);
+            per1.SayHello();
+            
+            NewPerson per2 = new NewPerson("Дэбилыч", 67);
+            
+            Console.WriteLine(per2.Age);
+            per2.Age = 20;
+
+            per2.SayHello();
+        }
+    }
+}
